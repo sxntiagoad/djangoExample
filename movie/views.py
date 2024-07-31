b48482bf862bcfd69acc1d4ebafb1390e39feac1
sxntiagoad <salvarezd3@eafit.edu.co>
2024-07-31 17:33:43 -0500

    busqueda peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Movie
 # Create your views here.
 def home(request):
-    return render(request,'home.html',{'name':'Santiago'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.all()
+    else:
+        movies = Movie.objects.all()
+    return render(request,'home.html',{'searchTerm':searchTerm, 'movies': movies})
 def about(request):
     return render(request,'about.html')
